351a77e3f54d5ca1774b7d6e4373909ab3a54cb0
Ayamohamedkhalil <ayak78118@gmail.com>
2025-02-14 17:52:16 +0200

    comment to problem 6

--- Task-2/Problem-6.py
+++ Task-2/Problem-6.py
@@ -1,6 +1,6 @@
 import random
 import string
-
+# Using "_" is refer to  name of variable isn't important 
 password = ""
 for _ in range(8):
     password += random.choice(string.ascii_letters + string.digits)  
