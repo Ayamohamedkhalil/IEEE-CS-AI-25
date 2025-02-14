31e5331a4c8d993d702be3ec87107ea440721a92
Ayamohamedkhalil <ayak78118@gmail.com>
2025-02-14 17:41:56 +0200

    P5

---
+++ Task-2/Problem-5.py
@@ -0,0 +1,18 @@
+from collections import Counter
+import re
+
+# Read the file
+file_path = "Task-2\simple_text.txt"
+
+with open(file_path, "r", encoding="utf-8") as file:
+    text = file.read().lower()  
+
+
+words = re.findall(r"\b\w+'\w+|\w+\b", text)  # Handles contractions like "I'm"
+
+
+word_counts = Counter(words)
+
+
+for word, count in word_counts.items():
+    print(f"{word}: {count}")
